178e49c99154e8d888cfe25b388e5bd950cd71cf
RikiSem <112815362+RikiSem@users.noreply.github.com>
2023-02-01 17:08:41 +0300

    Update deleteUser.php

--- deleteUser.php
+++ deleteUser.php
@@ -4,6 +4,6 @@ $userInfo = [];
 include("sys/connect.php");
 $zapros = "DELETE FROM `userlist` WHERE `login`='$userLogin'";
 $result = mysqli_query($link,$zapros) or die(mysqli_error($link));
-$userInfo['status'] = "success";
+$userInfo['status'] = $result;
 echo(json_encode($userInfo));
 ?>
